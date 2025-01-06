87c532d888024b3f881e4d34a7ca6b6f63aaf93a
syed-imran-devops <imransyedstg@gmail.com>
2025-01-06 16:30:41 +0530

    hellow script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "imran:: hello irfan, how are you?"
+echo "irfan:: hi imran iam fn. how are you ?"
+echo "imran iam fine too, how is ur work "
+echo "irfan not bad, iam thinking to upgrade to devops"
