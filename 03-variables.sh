a3574bd96385a6d24e9142597338824609a5f24f
syed-imran-devops <imransyedstg@gmail.com>
2025-01-06 19:20:43 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=IMRAN
+PERSON2=IRFAN
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1 iam fn. how are you ?"
+echo "$PERSON1 iam fine too, how is ur work "
+echo "$PERSON2 not bad, iam thinking to upgrade to devops"
