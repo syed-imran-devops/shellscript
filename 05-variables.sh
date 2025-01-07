0126e0d0d42d42fbe2c77a1413cb91b072b66504
syed-imran-devops <imransyedstg@gmail.com>
2025-01-07 09:39:30 +0530

    helllo script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read USERNAME #here USERNAME is variable
+
+echo "please enter password::"
+
+read PASSWORD
+
+echo "username is: $USERNAME, password is: $PASSWORD"
