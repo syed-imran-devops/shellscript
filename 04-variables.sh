675f83fcf9f23e79117a5e9d8b56d5828b8b5538
syed-imran-devops <imransyedstg@gmail.com>
2025-01-09 21:55:02 +0530

    hellow script

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,4 +5,4 @@ PERSON2=$2
 echo "$PERSON1:: hello $PERSON2, how are you?"
 echo "$PERSON2:: hi $PERSON1 iam fn. how are you ?"
 echo "$PERSON1 iam fine too, how is ur work "
-echo "$PERSON2 not bad, iam thinking to upgrade to devops"
+echo "$PERSON2 not bad, already iam a devops engineer"
