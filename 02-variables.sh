9feaf9cd28ff1e5a0f31f233de9dd9b221b2ce21
syed-imran-devops <imransyedstg@gmail.com>
2025-01-09 20:18:44 +0530

    hellow script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 person1=imran
 person2=irfan
 
-echo "person1:hi person2 how r u ?"
-echo "person2:hlo person1 iam fine how about you"
-echo "person1:iam also gud person2, are you going to join in devops course?"
-echo "person2: no person1 iam already working as an devops engineer"
+echo "$person1:hi $person2 how r u ?"
+echo "$person2:hlo $person1 iam fine how about you"
+echo "$person1:iam also gud $person2, are you going to join in devops course?"
+echo "$person2: no $person1 iam already working as an devops engineer"
