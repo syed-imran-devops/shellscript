89dc3069228ebc0273a4c3c0770bbd9fe29f749e
syed-imran-devops <imransyedstg@gmail.com>
2025-01-10 11:13:49 +0530

    hellow script

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "all variables: $@"
-echo "number of variables passed: $#"
-echo "script name : $0"
-echo "current working dir : $pwd"
-echo "home dir of current user : $HOME"
-echo "which user is runnning this script : $USER"
-echo "hostname : $HOSTNAME"
-echo "process id of the current shell script : $$"
-
-sleep 60
-echo "process id of last background command: $!"
+echo "all variables $@"
