494660825ae76f0139acf81cdd3683b71d218065
syed-imran-devops <imransyedstg@gmail.com>
2025-01-10 12:19:23 +0530

    hellow script

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [$number -gt 10 ]
 then
-    echo "if number $NUMBER is greater than 10"
+    echo "given number $NUMBER is greater than 10"
 else
-    echo "if number $number is less than 10"
+    echo "given number $number is less than 10"
 fi
