094243752943fb6b8baf28fa8f5671925d3efacd
syed-imran-devops <imransyedstg@gmail.com>
2025-01-10 17:56:42 +0530

    hellow script

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,14 +2,8 @@
 
 userid=$(id -u)
 
-if [ $USERID -ne 0 ]
-then 
+if [$userid -ne 0]
+then
     echo "please run this script with root access."
-    exit 1 # manually exit if error comes.
-ELSE
-    echo "you are super user."
+
 FI
-
-dnf install mysql -y
-
-echo "is script proceeding?"
