f3963a1be0539e0cdcd2a7e8328ca36394bf4f1a
syed-imran-devops <imransyedstg@gmail.com>
2025-01-11 10:01:07 +0530

    hellow script

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(ECHO $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$script_name-$timestamp.log
 
 VALIDATE(){
